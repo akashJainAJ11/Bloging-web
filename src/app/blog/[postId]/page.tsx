d3cbf92f56db7c5c4429da502aa2720d3ac8a61e
akashJainAJ11 <akashjain100200@gmail.com>
2024-06-19 17:11:03 +0530

    type error solved in src/app/blog/[postId]/page.tsx file

--- src/app/blog/[postId]/page.tsx
+++ src/app/blog/[postId]/page.tsx
@@ -31,6 +31,7 @@ export default async function Page({ params }: any) {
     allowedTags: sanitizeHtml.defaults.allowedTags,
     allowedAttributes: sanitizeHtml.defaults.allowedAttributes,
   });
+  const authorName = post.author.name || 'Unknown Author';
 
   return (
     <section>
@@ -39,7 +40,7 @@ export default async function Page({ params }: any) {
         <div className="flex items-center mb-4">
           <div className="flex-shrink-0">
             <div className="bg-blue-500 text-white rounded-full h-8 w-8 flex items-center justify-center text-xl">
-              {post.author.name.charAt(0).toUpperCase()}
+              {authorName.charAt(0).toUpperCase()}
             </div>
           </div>
           <div className="ml-4">
